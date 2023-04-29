8cd76a55116fa82d324bbd0aa1fa1b1c47f57104
Matthew Day <matt@matthewday.net>
2023-04-29 12:44:31 -0400

    add auth middleware

---
+++ middleware/index.js
@@ -0,0 +1,10 @@
+function checkAuth(req, res, next) {
+  const { password, username } = req.body;
+  if (password === '1234' && username === 'raul') {
+    next();
+  } else {
+    res.json({isError: true});
+  }
+}
+
+module.exports = {checkAuth };
